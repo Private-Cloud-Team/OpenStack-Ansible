b60e98734b945b377192d7810e8227939a61dd52
Zakariaa Oulhafiane <zakariaa.oulhafiane@gmail.com>
2022-03-10 14:24:36 +0100

    Fixing: couldn't resolve module/action 'openstack.config_template.config_template', by installing community collections

--- scripts/run_playbook.sh
+++ scripts/run_playbook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /vagrant/playbooks
 ansible-galaxy install -r requirements.yml -p ./roles
-ansible-galaxy collection install community.general
+ansible-galaxy install -r collection-requirements.yml
 ansible-playbook -i hosts.ini -u vagrant install_openstack.yml
