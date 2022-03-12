8f11e8e2284e327378b616c655272777aeb97368
ouzamhar <ouzamhar@oumaimas-MacBook-Pro.local>
2022-03-12 15:13:26 +0100

    Run playbook with vagrant up

--- scripts/run_playbook.sh
+++ scripts/run_playbook.sh
@@ -2,4 +2,4 @@
 cd /vagrant/playbooks
 ansible-galaxy install -r requirements.yml -p ./roles
 ansible-galaxy install -r collection-requirements.yml
-#ansible-playbook -i hosts.ini -u vagrant install_openstack.yml
+ansible-playbook -i hosts.ini -u vagrant install_openstack.yml
