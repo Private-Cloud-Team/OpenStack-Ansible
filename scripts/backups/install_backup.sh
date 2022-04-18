dbf04ba7bbc618e31e66d8d82f970f1f4b46a520
Zakariaa Oulhafiane <zakariaa.oulhafiane@gmail.com>
2022-04-18 13:07:06 +0000

    Update install_backup.sh

--- scripts/backups/install_backup.sh
+++ scripts/backups/install_backup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "===Installing LVM Snapshot Restore & Backup Method==="
-echo "Setting up crontab..."
-echo "@reboot touch /root/itsnotok;sleep 600;bash /root/backup.sh >> ~/logs" | crontab -
 echo "Set size of LVM snapshot (example: 20G):"
 read size
 echo "Creating LVM Snapshot..."
 lvcreate -L $size -s -n snap /dev/ubuntu-vg/ubuntu-lv
+echo "Setting up crontab..."
+echo "@reboot touch /root/itsnotok;sleep 600;bash /root/backup.sh >> ~/logs" | crontab -
 echo "Setting backup.sh && check.sh..."
 cp backup.sh /root/backup.sh
 cp check.sh /root/check.sh
