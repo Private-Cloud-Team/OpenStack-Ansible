f08d0f67b53781a45d0d6f6678d51b4e5ef8ec75
Zakariaa Oulhafiane <zakariaa.oulhafiane@gmail.com>
2022-04-26 18:12:55 +0000

    Now check.sh run after snapshot merging.

--- scripts/backups/install_backup.sh
+++ scripts/backups/install_backup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 lvchange --refresh ubuntu-vg
 echo "===Installing LVM Snapshot Restore & Backup Method==="
+cp check.sh /root/check.sh
+echo "Setting .bash_profile to run check.sh"
+cat ~/.bashrc | grep "bash /root/check.sh" || echo "bash /root/check.sh" >> /root/.bashrc
 echo "Set size of LVM snapshot (example: 20G):"
 read size
 echo "Creating LVM Snapshot..."
@@ -10,11 +13,7 @@ if [ $check != 0 ];then
     echo -e '\033[31mAn error occurs. \033[0m'
     exit
 fi
+cp backup.sh /root/backup.sh
 echo "Setting up crontab..."
 echo "@reboot touch /root/itsnotok;sleep 600;bash /root/backup.sh >> ~/logs" | crontab -
-echo "Setting backup.sh && check.sh..."
-cp backup.sh /root/backup.sh
-cp check.sh /root/check.sh
-echo "Setting .bash_profile to run check.sh"
-cat ~/.bashrc | grep "bash /root/check.sh" || echo "bash /root/check.sh" >> /root/.bashrc
 echo "LVM Snapshot creation finished"
